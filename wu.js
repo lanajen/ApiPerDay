16f683e645c6bef231b6d9419040ceadc4ad9211
Lana Lee <lanalee@Lanas-MacBook-Pro.local>
2016-06-03 16:17:19 -0700

    crap“

--- wu.js
+++ wu.js
@@ -1,21 +1,22 @@
+$(document).ready(function () {
+	var city;
 
-var city;
+	$('#clicking').on('click', function (e) {
+		city = prompt("What Californian city would you like to look up?");
+		city = city.replace(/ /g,"_");
+	)}
 
-function weathering() {
-	city = prompt("What Californian city would you like to look up?");
-	city = city.replace(/ /g,"_");
-}
-
-$.ajax({
-	url : "http://api.wunderground.com/api/0def10027afaebb7/conditions/q/CA/"+city+".json",
-	dataType : "jsonp",
-	success : function(parsed_json) {
-		var location = parsed_json['location']['city'];
-		var temp_f = parsed_json['current_observation']['feelslike_f'];
-		var look = parsed_json['current_observation']['weather'];
-		alert("Right now in " + location + ", it is "+look+" and feels like " + feelslike_f+" degrees F.");
-	}
-});
+	$.ajax({
+		url : "http://api.wunderground.com/api/0def10027afaebb7/conditions/q/CA/"+city+".json",
+		dataType : "jsonp",
+		success : function(parsed_json) {
+			var location = parsed_json['location']['city'];
+			var temp_f = parsed_json['current_observation']['feelslike_f'];
+			var look = parsed_json['current_observation']['weather'];
+			alert("Right now in " + location + ", it is "+look+" and feels like " + feelslike_f+" degrees F.");
+		}
+	})
+)}
 
 // $(document).ready(function($) {
 //   $.ajax({
