dc496938344a2c0dffd1be1d94dbac8905a96547
Lana Lee <ljelee@gmail.com>
2016-06-03 03:31:09 -0700

    Add files via upload

---
+++ wu.js
@@ -0,0 +1,31 @@
+
+var city;
+
+function weathering() {
+	city = prompt("What Californian city would you like to look up?");
+	city = city.replace(/ /g,"_");
+}
+
+$.ajax({
+	url : "http://api.wunderground.com/api/0def10027afaebb7/conditions/q/CA/"+city+".json",
+	dataType : "jsonp",
+	success : function(parsed_json) {
+		var location = parsed_json['location']['city'];
+		var temp_f = parsed_json['current_observation']['feelslike_f'];
+		var look = parsed_json['current_observation']['weather'];
+		alert("Right now in " + location + ", it is "+look+" and feels like " + feelslike_f+" degrees F.");
+	}
+});
+
+// $(document).ready(function($) {
+//   $.ajax({
+//   url : "http://api.wunderground.com/api/0def10027afaebb7/conditions/q/CA/"+city+".json",
+//   dataType : "jsonp",
+//   success : function(parsed_json) {
+//   var location = parsed_json['location']['city'];
+//   var temp_f = parsed_json['current_observation']['feelslike_f'];
+//   var look = parsed_json['current_observation']['weather'];
+//   alert("Right now in " + location + ", it is "+look+" and feels like " + feelslike_f+" degrees F.");
+//   }
+//   });
+// });
